b727090a93929db6588b68e66d6d35e4af1584ae
МалевАВ <@106-01>
2023-03-21 16:50:10 +0300

    Добавьте файлы проекта.

---
+++ Sessia2Malev/Calculations.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sessia2Malev
+{
+    public class Calculations
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            TimeSpan time = beginWorkingTime;
+
+            TimeSpan consultation = new TimeSpan(0, consultationTime, 0);
+
+            string[] mistake = new string[1];
+
+            int count = 0;
+
+            int countStartTime = durations.Length;
+
+            int j = 0;
+
+            int k = 0;
+
+            string[] periods = new string[count];
+
+            if (beginWorkingTime > endWorkingTime)
+            {
+                mistake[0] = "ошибка";
+
+                return mistake;
+            }
+
+            while (time != endWorkingTime)
+            {
+                time = time + consultation;
+
+                count++;
+            }
+
+            if (beginWorkingTime > startTimes[0])
+            {
+                mistake[0] = "ошибка";
+
+                return mistake;
+            }
+
+            else if (endWorkingTime < startTimes[countStartTime - 1])
+            {
+                mistake[0] = "ошибка";
+
+                return mistake;
+            }
+
+            for (int i = 0; i < countStartTime; i++)
+            {
+                if (i == countStartTime - 1)
+                {
+                    if (startTimes[countStartTime - 2] > startTimes[countStartTime - 1])
+                    {
+                        mistake[0] = "ошибка";
+
+                        return mistake;
+                    }
+                }
+
+                else
+                {
+                    if (startTimes[i] > startTimes[i + 1])
+                    {
+                        mistake[0] = "ошибка";
+
+                        return mistake;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i <= count; i++)
+            {
+                if (beginWorkingTime != endWorkingTime)
+                {
+                    if (beginWorkingTime + consultation <= endWorkingTime)
+                    {
+                        if (j != countStartTime)
+                        {
+                            TimeSpan duration = new TimeSpan(0, durations[j], 0);
+
+                            if (beginWorkingTime == startTimes[j])
+                            {
+                                if (endWorkingTime == startTimes[j])
+                                {
+                                    mistake[0] = "ошибка";
+
+                                    return mistake;
+                                }
+
+                                else
+                                {
+                                    beginWorkingTime = beginWorkingTime + duration;
+
+                                    j++;
+                                }
+
+                            }
+
+                            else if (beginWorkingTime + consultation == beginWorkingTime)
+                            {
+                                beginWorkingTime = beginWorkingTime + duration;
+
+                                j++;
+                            }
+
+                            else if (beginWorkingTime + consultation > startTimes[j] && startTimes[j] >= beginWorkingTime)
+                            {
+                                beginWorkingTime = startTimes[j] + duration;
+
+                                j++;
+                            }
+
+                            else
+                            {
+                                periods[k] = string.Format("{0:hh\\:mm}-{1:hh\\:mm}", beginWorkingTime, beginWorkingTime + consultation);
+
+                                beginWorkingTime = beginWorkingTime + consultation;
+
+                                k++;
+                            }
+                        }
+
+                        else
+                        {
+
+                            periods[k] = string.Format("{0:hh\\:mm}-{1:hh\\:mm}", beginWorkingTime, beginWorkingTime + consultation);
+
+                            beginWorkingTime = beginWorkingTime + consultation;
+
+                            k++;
+                        }
+                    }
+
+                    else
+                    {
+                        mistake[0] = "ошибка";
+
+                        return mistake;
+                    }
+                }
+            }
+
+            return periods;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Sessia2Malev;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ErrorInWrongOrder() // проверяет на вывод ошибки при неправильном распорядке записей на прием
+        {
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+
+            TimeSpan endWorkingTime = new TimeSpan(18, 00, 0);
+
+            TimeSpan timeSpan = new TimeSpan(8, 0, 0);
+
+            TimeSpan timeSpan1 = new TimeSpan(9, 0, 0);
+
+            TimeSpan timeSpan2 = new TimeSpan(16, 0, 0);
+
+            TimeSpan timeSpan3 = new TimeSpan(15, 30, 0);
+
+            TimeSpan timeSpan4 = new TimeSpan(16, 50, 0);
+
+            int[] durations = { 60, 30, 10, 10, 40 };
+
+            int consultationTime = 30;
+
+            TimeSpan[] startTimes = { timeSpan, timeSpan1, timeSpan2, timeSpan3, timeSpan4 };
+
+            string[] mistake = { "ошибка" };
+
+            string[] actual = Calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+            Assert.AreEqual(mistake[0], actual[0]);
+        }
+
+        [TestMethod]
+        public void ErrorsWhenWritingLaterThanWorking() // проверяет на вывод ошибки при записе позже чем рабочий
+        {
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+
+            TimeSpan endWorkingTime = new TimeSpan(18, 00, 0);
+
+            TimeSpan timeSpan = new TimeSpan(8, 0, 0);
+
+            TimeSpan timeSpan1 = new TimeSpan(9, 0, 0);
+
+            TimeSpan timeSpan2 = new TimeSpan(15, 0, 0);
+
+            TimeSpan timeSpan3 = new TimeSpan(15, 30, 0);
+
+            TimeSpan timeSpan4 = new TimeSpan(19, 50, 0);
+
+            int[] durations = { 60, 30, 10, 10, 40 };
+
+            int consultationTime = 30;
+
+            TimeSpan[] startTimes = { timeSpan, timeSpan1, timeSpan2, timeSpan3, timeSpan4 };
+
+            string[] mistake = { "ошибка" };
+
+            string[] actual = Calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+            Assert.AreEqual(mistake[0], actual[0]);
+        }
+
+        [TestMethod]
+        public void EarlyRecordingError() // проверяет на вывод ошибки при записе раньше чем рабочий
+        {
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+
+            TimeSpan endWorkingTime = new TimeSpan(18, 00, 0);
+
+            TimeSpan timeSpan = new TimeSpan(7, 0, 0);
+
+            TimeSpan timeSpan1 = new TimeSpan(9, 0, 0);
+
+            TimeSpan timeSpan2 = new TimeSpan(15, 0, 0);
+
+            TimeSpan timeSpan3 = new TimeSpan(15, 30, 0);
+
+            TimeSpan timeSpan4 = new TimeSpan(16, 50, 0);
+
+            int[] durations = { 60, 30, 10, 10, 40 };
+
+            int consultationTime = 30;
+
+            TimeSpan[] startTimes = { timeSpan, timeSpan1, timeSpan2, timeSpan3, timeSpan4 };
+
+            string[] mistake = { "ошибка" };
+
+            string[] actual = Calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+            Assert.AreEqual(mistake[0], actual[0]);
+        }
+
+        [TestMethod]
+        public void ErrorsIfTheEndOfTheWorkingDayIsEarlierThanTheBeginning() // проверяет на вывод ошибки если конец рабочего дня раньше чем начало
+        {
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+
+            TimeSpan endWorkingTime = new TimeSpan(7, 0, 0);
+
+            TimeSpan timeSpan = new TimeSpan(8, 0, 0);
+
+            TimeSpan timeSpan1 = new TimeSpan(9, 0, 0);
+
+            TimeSpan timeSpan2 = new TimeSpan(15, 20, 0);
+
+            TimeSpan timeSpan3 = new TimeSpan(15, 30, 0);
+
+            TimeSpan timeSpan4 = new TimeSpan(16, 50, 0);
+
+            int[] durations = { 60, 30, 20, 10, 40 };
+
+            int consultationTime = 30;
+
+            TimeSpan[] startTimes = { timeSpan, timeSpan1, timeSpan2, timeSpan3, timeSpan4 };
+
+            string[] mistake = { "ошибка" };
+
+            string[] actual = Calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+            Assert.AreEqual(mistake[0], actual[0]);
+        }
+    }
+}
